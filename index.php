93d3ae512cfe1c0c421edcf5320ede3a9ea96d2a
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-31 16:11:21 +0100

    Created class "categoria"

--- index.php
+++ index.php
@@ -6,6 +6,21 @@
         public $price;
         public $img;
 
+        // Definisco il costruttore della classe prodotto
+        function __construct($_name, $_price, $_img){
+            $this -> name = $_name;
+            $this -> price = $_price;
+            $this -> img = $_img;
+        }
+        
+    }
 
+    // Definisco la classe "categoria" per aggiungerla alla classe "prodotto" attraverso PHP composition
+    class Categoria{
+        public $animal;
+
+        function __construct($_animal){
+            $this -> animal = $_animal;
+        }
     }
 ?>

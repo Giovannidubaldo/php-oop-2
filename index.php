d18c0198e56ed2af1f905899f8418d01962e5b99
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-31 16:35:39 +0100

    Added class "accessori", son of class "product"

--- index.php
+++ index.php
@@ -5,14 +5,28 @@
         public $name;
         public $price;
         public $img;
+        public $category;
 
         // Definisco il costruttore della classe prodotto
-        function __construct($_name, $_price, $_img){
+        function __construct($_name, $_price, $_img, Categoria $_category){
             $this -> name = $_name;
             $this -> price = $_price;
             $this -> img = $_img;
+            $this -> category = $_category;
         }
         
+    }
+
+    // Definisco la sottoclasse "Accessori", figlia della classe "prodotto"
+    class Accessori extends Prodotto{
+        $type;
+        $description;
+
+        function __construct($_name, $_price, $_img, $_category, $_type, $_description){
+            parent::__construct($_name, $_price, $_img, $_category);
+            $this -> type = $_type;
+            $this -> description = $_description;
+        }
     }
 
     // Definisco la classe "categoria" per aggiungerla alla classe "prodotto" attraverso PHP composition
@@ -23,4 +37,11 @@
             $this -> animal = $_animal;
         }
     }
+
+    // Creo istanze della classe Categoria
+    $cane = new Categoria('Cane');
+    $gatto = new Categoria('Gatto');
+    $pesci = new Categoria('Pesci');
+    $uccelli = new Categoria('Uccelli');
+    
 ?>
